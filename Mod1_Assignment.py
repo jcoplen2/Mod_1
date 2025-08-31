462c5e68e71c21912b6ade469930946cfd753a35
jessi <jcoplen2@ivytech.edu>
2025-08-31 09:48:09 -0400

    Completed assignment

--- Mod1_Assignment.py
+++ Mod1_Assignment.py
@@ -1,13 +1,17 @@
 #access control
 #author jbc
-#last updated 8/27/2025
-#this app shows how access control is implemented
+#last updated 8/31/2025
+#this app shows how access control is implemented. It provides confidentiality via
+#a login. The data integrity is protected by access control. There are no issues
+#with availability. 
 
-
+#create list
 students = ["James", "Peter", "Julie", "Jake"]
 
+#Simulate login
 Username = input("Please enter User or Admin: ")
 
+#Admin only function
 def updateList():
     if Username == "Admin":
         students.append(name)
@@ -16,6 +20,20 @@ def updateList():
     else:
         print("You do not have permission for this.")
 
-name = input("Please enter student name to add to the list: ")
+#Student only function
+def viewList():
+    if Username == "User":
+        print("Here is a list of the students:")
+        print(students)
+    else:
+        print("You do not have permission for this.")
 
-updateList()
+#Control access
+if Username == "Admin":
+    name = input("Please enter student name to add to the list: ")
+    updateList()
+elif Username == "User":
+    viewList()
+else:
+    print("Invalid Role")
+
